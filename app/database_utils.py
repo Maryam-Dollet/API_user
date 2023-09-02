985aebcbd7ba4a29612c42e1cc0fc80d41be55c4
Maryam-Dollet <maryam.dollet@gmail.com>
2023-09-02 17:24:59 +0200

    update database connection function & gitignore

--- app/database_utils.py
+++ app/database_utils.py
@@ -1,23 +1,27 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import os
+import time
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
 def db_connection():
-    try:
-        connection = psycopg2.connect(
-            host="localhost",
-            database=os.getenv("DATABASE"),
-            user=os.getenv("USER"),
-            password=os.getenv("PASSWORD"),
-            port="5432",
-            cursor_factory=RealDictCursor,
-        )
-        cursor = connection.cursor()
-        print("Database connection was succeful !")
-    except Exception as error:
-        print("Connecting to Database failed...")
-        print("Error : ", error)
+    while True:
+        try:
+            connection = psycopg2.connect(
+                host=os.getenv("HOST"),
+                database=os.getenv("DATABASE"),
+                user=os.getenv("USER"),
+                password=os.getenv("PASSWORD"),
+                port=os.getenv("PORT"),
+                cursor_factory=RealDictCursor,
+            )
+            cursor = connection.cursor()
+            print("Database connection was succeful \U0001F44C !")
+            break
+        except Exception as error:
+            print("Connecting to Database failed...")
+            print("Error : ", error)
+            time.sleep(2)
