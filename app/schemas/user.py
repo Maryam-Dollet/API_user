772c95b16a2f913fbc5c49c7048783b17d6f3e93
Maryam-Dollet <maryam.dollet@gmail.com>
2023-09-03 15:41:22 +0200

    update user response model

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, EmailStr
 from uuid import UUID
+from datetime import datetime
 
 
 class UserCreate(BaseModel):
@@ -10,6 +11,7 @@ class UserCreate(BaseModel):
 class UserOut(BaseModel):
     user_id: UUID
     email: EmailStr
+    created_at: datetime
 
     class Config:
         from_attributes = True
