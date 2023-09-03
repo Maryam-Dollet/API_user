9540aa19df511cf12014faa61e4ebbf2ff127e1f
Maryam-Dollet <maryam.dollet@gmail.com>
2023-09-03 11:21:55 +0200

    create database connection with sqlalchemy

--- app/database_utils.py
+++ app/database_utils.py
@@ -1,3 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 import psycopg2
 from psycopg2.extras import DictCursor, RealDictCursor
 import os
@@ -5,6 +9,19 @@ import time
 from dotenv import load_dotenv
 
 load_dotenv()
+
+DATABASE = os.getenv("DATABASE")
+USER = os.getenv("USER")
+HOST = os.getenv("HOST")
+PASSWORD = os.getenv("PASSWORD")
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}/{DATABASE}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
 
 
 def db_connection():
