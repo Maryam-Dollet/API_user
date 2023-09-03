20f94d8dd5d20108be43980dd7a342dd1e8d77e6
Maryam-Dollet <maryam.dollet@gmail.com>
2023-09-03 13:07:38 +0200

    update database creation

--- app/models.py
+++ app/models.py
@@ -1,13 +1,14 @@
 from database_utils import Base
 from sqlalchemy.dialects.postgresql import UUID
-import uuid
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, text
 
 
 class Character(Base):
     __tablename__ = "characters"
-    character_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid1)
+    character_id = Column(
+        UUID(as_uuid=True), primary_key=True, server_default=text("uuid_generate_v1()")
+    )
     name = Column(String, nullable=False)
     occupation = Column(String, nullable=False)
     age = Column(Integer, nullable=True)
-    affiliation = Column(String, nullable=True, default="Not Provided")
+    affiliation = Column(String, nullable=True, server_default="Not Provided")
