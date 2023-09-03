0fbab5a6dbc7b1eed56bd899c4c17f5b19aab5f5
Maryam-Dollet <maryam.dollet@gmail.com>
2023-09-03 16:14:30 +0200

    update router

--- app/main.py
+++ app/main.py
@@ -2,14 +2,14 @@ from fastapi import FastAPI
 
 import models
 from database_utils import engine
-from routers import character_route, user_route
+from routers import character_router, user_router
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-app.include_router(character_route.router)
-app.include_router(user_route.router)
+app.include_router(character_router.router)
+app.include_router(user_router.router)
 
 
 @app.get("/")
