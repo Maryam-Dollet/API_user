950517c648e99d162c91755af2aee4eee8575dff
Maryam-Dollet <maryam.dollet@gmail.com>
2023-09-03 16:13:48 +0200

    include router to main

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 
 import models
-from database_utils import engine, get_db
-from utils import hash
+from database_utils import engine
+from routers import character_route, user_route
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.include_router(character_route.router)
+app.include_router(user_route.router)
 
 
 @app.get("/")
