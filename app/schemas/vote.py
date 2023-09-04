b54e974a8736e0c17593ffb892e00dbaeb3d909a
Maryam-Dollet <maryam.dollet@gmail.com>
2023-09-04 16:44:31 +0200

    add vote endpoint

---
+++ app/schemas/vote.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+from pydantic.types import conint
+from uuid import UUID
+
+
+class Vote(BaseModel):
+    character_id: UUID
+    dir: conint(gt=-1, le=1)
+    user_id: UUID
