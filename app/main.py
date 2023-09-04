b16dfcee04170d0133593d6f554b0c8ad859d9e5
Maryam-Dollet <maryam.dollet@gmail.com>
2023-09-04 16:45:38 +0200

    add vote router

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 import models
 from database_utils import engine
-from routers import character_router, user_router, auth
+from routers import character_router, user_router, auth, vote_router
 
 # from config import settings
 
@@ -14,6 +14,7 @@ app = FastAPI()
 app.include_router(character_router.router)
 app.include_router(user_router.router)
 app.include_router(auth.router)
+app.include_router(vote_router.router)
 
 
 @app.get("/")
