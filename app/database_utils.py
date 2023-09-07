42fcbc837b0166277e782d0893aaa265011865d6
Maryam-Dollet <maryam.dollet@gmail.com>
2023-09-07 21:31:44 +0200

    update create database if not exists

--- app/database_utils.py
+++ app/database_utils.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import database_exists, create_database
 import os
 from dotenv import load_dotenv
 
@@ -16,6 +17,8 @@ PASSWORD = os.getenv("PASSWORD")
 SQLALCHEMY_DATABASE_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}/{DATABASE}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+if not database_exists(engine.url):
+    create_database(engine.url)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
